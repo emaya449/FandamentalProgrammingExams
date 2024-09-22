bbcfc8085730d578dbcc50aca78801be39cd3aa3
EmmaMayanja <yanjatotti@gmail.com>
2024-09-22 06:50:32 -0500

    Add files via upload

---
+++ WhileLoopAns.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {//the code in the loop will run, over and over again, as long as a variable (i) is less than 5:
+  int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
+  return 0;
+}
+
